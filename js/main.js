bd6a782a1e678a761f68eb8e9788d2d4c4e18c9d
erreinoso <eramirezreinoso@gmail.com>
2020-06-30 09:30:59 +0200

    First condition

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
 'use strict';
+
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+
+const random = getRandomNumber(100);
+console.log(random);
+
+//Guardamos donde queremos escribir el texto
+const clue = document.querySelector('.js-clueText');
+const count = document.querySelector('.js-countNumber');
+const button = document.querySelector('.js-tryButton');
+
+//Guardamos el dato del/a usuario/a
+const userinput = document.querySelector('.js-userNumber');
+
+function handleinput() {
+  console.log(userinput.value);
+  const userinputnumber = parseInt(userinput.value);
+
+  console.log(userinputnumber);
+  if (userinputnumber === random) {
+    clue.innerHTML = '¡Has ganado, campeona!';
+  } else if (userinputnumber > random && userinputnumber <= 100) {
+    clue.innerHTML = 'Demasiado alto';
+  } else if (userinputnumber < random) {
+    clue.innerHTML = 'Demasiado bajo';
+  } else if (userinputnumber > random && userinputnumber > 100) {
+    clue.innerHTML = 'El número debe estar comprendido entre 1 y 100';
+  }
+}
+
+//listener
+button.addEventListener('click', handleinput);
