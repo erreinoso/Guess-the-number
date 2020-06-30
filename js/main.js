e2a655f4bc8019cfb6d263b544d66b58ea0e4a9e
erreinoso <eramirezreinoso@gmail.com>
2020-06-30 09:55:56 +0200

    form button able

--- js/main.js
+++ js/main.js
@@ -1,40 +1,44 @@
 'use strict';
-
+//Obtenemos número aleatorio, máximo 100
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
-
 const random = getRandomNumber(100);
 console.log(random);
 
-//Guardamos donde queremos escribir el texto
+//Guardamos dónde queremos escribir el texto
 const clue = document.querySelector('.js-clueText');
 const count = document.querySelector('.js-countNumber');
+
+//Guardamos dónde va a producirse el evento
 const button = document.querySelector('.js-tryButton');
 
 //Guardamos el dato del/a usuario/a
 const userinput = document.querySelector('.js-userNumber');
+
 //Número de intentos
 let attemps = 0;
 
 //funciones handle
 function handleGame() {
-  handleinput();
+  handleInput();
   handleCounter();
 }
 
-function handleinput() {
-  console.log(userinput.value);
+function handleInput() {
   const userinputnumber = parseInt(userinput.value);
   console.log(userinputnumber);
   if (userinputnumber === random) {
     clue.innerHTML = '¡Has ganado, campeona!';
-    attemps = 0;
+    // attemps = 0; Depende de la interpretación
   } else if (userinputnumber > random && userinputnumber <= 100) {
     clue.innerHTML = 'Demasiado alto';
-  } else if (userinputnumber < random) {
+  } else if (userinputnumber < random && userinputnumber > 0) {
     clue.innerHTML = 'Demasiado bajo';
-  } else if (userinputnumber > random && userinputnumber > 100) {
+  } else if (
+    (userinputnumber > random && userinputnumber > 100) ||
+    (userinputnumber < random && userinputnumber <= 0)
+  ) {
     clue.innerHTML = 'El número debe estar comprendido entre 1 y 100';
   }
 }
